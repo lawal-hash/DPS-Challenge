91d71e5505c044c1b82e47a4f94304461d14c3f8
Sophia <lawalsophia@gmail.com>
2022-03-15 16:42:09 +0000

    Adds Suptitle

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import visualization as vs
 import matplotlib.pyplot as plt
 import numpy as np
+
 if __name__ == '__main__':
     dataset, categories = vs.load_dataset('monatszahlen2112_verkehrsunfaelle.csv')
     category_dict, unique_category_dict = vs.category(dataset, categories)
     fig, axs = plt.subplots(len(categories), sharex=True)
     fig.set_size_inches(18.5, 10.5)
+    plt.suptitle('Visualisation of the Number of Accidents Per Category')
     for i in range(len(categories)):
         vs.visualization(category_dict, unique_category_dict, categories[i], axs[i])
     # plt.show()

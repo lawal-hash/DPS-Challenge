af46e96638f0c6a1a62003fa4db846d6c4bdc547
Sophia <lawalsophia@gmail.com>
2022-03-15 16:32:42 +0000

    Adds Image re-save option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import visualization as vs
 import matplotlib.pyplot as plt
-
+import numpy as np
 if __name__ == '__main__':
     dataset, categories = vs.load_dataset('monatszahlen2112_verkehrsunfaelle.csv')
     category_dict, unique_category_dict = vs.category(dataset, categories)
@@ -9,4 +9,5 @@ if __name__ == '__main__':
     for i in range(len(categories)):
         vs.visualization(category_dict, unique_category_dict, categories[i], axs[i])
     # plt.show()
-    plt.savefig('Images/MONATSZAHL.png')
+    copy = np.random.randint(100, size=1)
+    plt.savefig(f'Images/MONATSZAHL_{int(copy)}.png')

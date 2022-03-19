49f1d4559726e8f745882b382eaa39503be04024
Sophia <lawalsophia@gmail.com>
2022-03-19 19:08:05 +0000

    Adds more exception to handle

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, request
 import json
 from datetime import datetime
 import statsmodels.api as sm
-from model import load_data
+from util import load_data
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def get_forecast(year, month):
@@ -13,7 +16,7 @@ def get_forecast(year, month):
     datetime_end_date = datetime.strptime(end_date, '%Y-%m-%d')
     time_delta = (datetime_forcast_date.year - datetime_end_date.year) * 12 + (
                 datetime_forcast_date.month - datetime_end_date.month)
-    arima_model = sm.tsa.arima.ARIMA(data_df['WERT'], order=(49, 0, 10), trend='ct')
+    arima_model = sm.tsa.arima.ARIMA(data_df['WERT'], seasonal_order=(5, 0, 6, 12), trend='c')
     time_series = arima_model.fit()
     forecast = time_series.forecast(time_delta)
     return forecast[-1]
@@ -33,11 +36,9 @@ def welcome():
 
 @app.route('/forecast', methods=['POST'])
 def forecast_accident():
-    # data = request.data
-    print(request.data)
     try:
         parsed = json.loads(request.data)
-        print(parsed)
+        #print(parsed)
         year = parsed.get('year')
         month = parsed.get('month')
 
@@ -46,7 +47,7 @@ def forecast_accident():
 
         # @TODO
         forecast = get_forecast(year, month)
-        return {'prediction': forecast}
+        return {'prediction': round(forecast, 2)}
 
-    except Exception:
-        return {'error': 'invalid data'}
+    except  (NameError, ValueError) as ex:
+        return {'error': 'Invalid data'}

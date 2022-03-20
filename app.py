43119bececfa9899fd5c140a6f1cff519ef88840
Sophia <lawalsophia@gmail.com>
2022-03-20 01:45:15 +0000

    Separates training and inference

--- app.py
+++ app.py
@@ -7,38 +7,32 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-
-def get_forecast(year, month):
-    forcast_date = str(year) + '-' + str(month) + '-' + '01'
-    end_date = '2020-12-01'
-    data_df = load_data('Alkoholunfälle_insgesamt.csv')
-    datetime_forcast_date = datetime.strptime(forcast_date, '%Y-%m-%d')
-    datetime_end_date = datetime.strptime(end_date, '%Y-%m-%d')
-    time_delta = (datetime_forcast_date.year - datetime_end_date.year) * 12 + (
-                datetime_forcast_date.month - datetime_end_date.month)
-    arima_model = sm.tsa.arima.ARIMA(data_df['WERT'], seasonal_order=(5, 0, 6, 12), trend='c')
-    time_series = arima_model.fit()
-    forecast = time_series.forecast(time_delta)
-    return forecast[-1]
-
-
 app = Flask(__name__)
 
 
-def get_prediction(year, month):
-    return year + month
+def fit_model():
+    data_df = load_data('Alkoholunfälle_insgesamt.csv')
+    arima_model = sm.tsa.arima.ARIMA(data_df['WERT'], seasonal_order=(5, 0, 6, 12), trend='c')
+    time_series = arima_model.fit()
+    return time_series
 
 
-@app.route('/')
-def welcome():
-    return "<p>Hello, World!</p>"
+model = fit_model()
+print(model)
+
+
+def get_forecast(year, month):
+    forecast_date = str(year) + '-' + str(month) + '-' + '01'
+    end_date = '2020-12-01'
+    forecast = model.predict(start=end_date, end=forecast_date)
+    return forecast
 
 
 @app.route('/forecast', methods=['POST'])
 def forecast_accident():
     try:
         parsed = json.loads(request.data)
-        #print(parsed)
+        print(parsed)
         year = parsed.get('year')
         month = parsed.get('month')
 
@@ -47,7 +41,9 @@ def forecast_accident():
 
         # @TODO
         forecast = get_forecast(year, month)
-        return {'prediction': round(forecast, 2)}
+        print(forecast)
+        forecast = forecast[-1]
+        return {'prediction': round(forecast,2)}
 
-    except  (NameError, ValueError) as ex:
+    except (NameError, ValueError) as ex:
         return {'error': 'Invalid data'}

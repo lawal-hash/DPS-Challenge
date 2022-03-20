c6cc904e1cf7c1b9e7f60580b214cf92d623c97d
Sophia <lawalsophia@gmail.com>
2022-03-20 05:48:50 +0000

    Updates model inferences

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 import json
-from datetime import datetime
 import statsmodels.api as sm
-from util import load_data
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -10,22 +8,11 @@ warnings.filterwarnings("ignore")
 app = Flask(__name__)
 
 
-def fit_model():
-    data_df = load_data('Alkoholunfälle_insgesamt.csv')
-    arima_model = sm.tsa.arima.ARIMA(data_df['WERT'], seasonal_order=(5, 0, 6, 12), trend='c')
-    time_series = arima_model.fit()
-    return time_series
-
-
-#model = fit_model()
-#print(model)
-
-
 def get_forecast(year, month):
     forecast_date = str(year) + '-' + str(month) + '-' + '01'
     end_date = '2020-12-01'
-    #forecast = model.predict(start=end_date, end=forecast_date)
-    forecast = 0
+    model = sm.load('forecast_model.pickle')
+    forecast = model.predict(start=end_date, end=forecast_date)
     return forecast
 
 
@@ -43,8 +30,8 @@ def forecast_accident():
         # @TODO
         forecast = get_forecast(year, month)
         print(forecast)
-        #forecast = forecast[-1]
-        return { } #{'prediction': round(forecast,2)}
+        forecast = forecast[-1]
+        return {'prediction': round(forecast, 2)}
 
-    except (NameError, ValueError) as ex:
+    except (NameError, ValueError):
         return {'error': 'Invalid data'}
